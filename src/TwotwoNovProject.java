96ca34929435bddf15daea4e8037b7d5f3596799
stellagood <117808030+stellagood@users.noreply.github.com>
2022-11-09 18:10:27 +0800

    project 5

---
+++ src/TwotwoNovProject.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+public class TwotwoNovProject {
+
+    void bubblesort(int[] nums){
+        int len = nums.length;
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        for(int i=0; i<len; i++)
+        {
+            stack1.push(nums[i]);
+        }
+
+        for(int i=0; i<len-1; i++)
+        {
+            stack2.push(stack1.pop());
+            for(int j=0; j<len-i-1; j++)
+            {
+                while(!stack1.isEmpty())
+                    if(stack1.peek()>stack2.peek())
+                    {
+                        stack2.push(stack1.pop());
+                    }
+                    else
+                    {
+                        int temp = stack2.pop();
+                        stack2.push(stack1.pop());
+                        stack2.push(temp);
+                    }
+            }
+
+            while(!stack2.isEmpty())
+            {
+                stack1.push(stack2.pop());
+            }
+        }
+
+        int n=0;
+        while(!stack1.isEmpty())
+        {
+            nums[n]=stack1.pop();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] num = {7, 2, 12, 8, 5};
+        TwotwoNovProject oldbubblesort = new TwotwoNovProject();
+        oldbubblesort.bubblesort(num);
+        System.out.println(Arrays.toString(num));
+    }
+}
